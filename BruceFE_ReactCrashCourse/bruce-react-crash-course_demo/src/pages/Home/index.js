ba6d05bd77203294596d25a7262a5bdd381cb543
郵差 <twHenryYang@gmail.com>
2022-01-03 17:37:48 +0800

    Import components & css

--- BruceFE_ReactCrashCourse/bruce-react-crash-course_demo/src/pages/Home/index.js
+++ BruceFE_ReactCrashCourse/bruce-react-crash-course_demo/src/pages/Home/index.js
@@ -5,9 +5,19 @@
 
 import React from "react";
 // 新版本的React有優化過了，上面這行可以不加
+import Edit from "./components/Edit"
+import List from "./components/List"
+import Item from "./components/Item"
+
+import "./index.css"
 
 const Home = () => {
-    return <div>Home</div>
+    return <div>
+        Home
+        <Edit />
+        <List />
+        <Item />
+    </div>
 }
 
 export default Home
